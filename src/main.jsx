ab67bf0ee48f2de443805571b20cdbcfe5b64d0b
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-06-29 14:10:55 +0000

    Setup LandingScreen

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingScreen from './screens/LandingScreen.jsx'
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <LandingScreen />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
---
+++ src/screens/LandingScreen.jsx
@@ -0,0 +1,12 @@
+
+const LandingScreen = () => {
+  return (
+    <>
+        <header className="bg-gradient-to-r from">
+
+        </header>
+    </>
+  )
+}
+
+export default LandingScreen
