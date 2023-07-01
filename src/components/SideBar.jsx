9db82db15a352b47d8665df61c804129d1e360e1
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-07-01 07:31:52 +0000

    Added slide in animation to SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,30 +1,62 @@
 import { useContext } from "react";
 import Category from "./Category";
+import PropTypes from "prop-types";
 import { FeedbackContext } from "../context/FeedbackContext";
 
-const SideBar = () => {
-  const {setFilterBy, selected } = useContext(FeedbackContext)
+const SideBar = ({ showSideBar }) => {
+  const { setFilterBy, selected } = useContext(FeedbackContext);
   const categoryHandler = (category) => {
-    setFilterBy(category)
-  }
+    setFilterBy(category);
+  };
   return (
-    <div className="absolute top-full right-0 z-10 p-4 bg-grayTheme flex flex-col h-screen">
+    <div
+      className={`absolute top-0 right-0 z-10 p-4 bg-grayTheme flex flex-col h-screen ${
+        showSideBar ? "translate-x-0" : "translate-x-full"
+      } ease-in-out duration-300`}
+    >
       <div className="flex flex-col bg-white rounded-xl">
         <div className="flex flex-col p-7 pb-10 gap-4">
           <div className="flex gap-4">
-            <Category selected={selected} onClick={categoryHandler} category={"All"} />
-            <Category selected={selected} onClick={categoryHandler} category={"UI"} />
-            <Category selected={selected} onClick={categoryHandler} category={"UX"} />
+            <Category
+              selected={selected}
+              onClick={categoryHandler}
+              category={"All"}
+            />
+            <Category
+              selected={selected}
+              onClick={categoryHandler}
+              category={"UI"}
+            />
+            <Category
+              selected={selected}
+              onClick={categoryHandler}
+              category={"UX"}
+            />
           </div>
           <div className="flex gap-4">
-            <Category selected={selected} onClick={categoryHandler} category={"Enhancement"} />
-            <Category selected={selected} onClick={categoryHandler} category={"Bug"} />
+            <Category
+              selected={selected}
+              onClick={categoryHandler}
+              category={"Enhancement"}
+            />
+            <Category
+              selected={selected}
+              onClick={categoryHandler}
+              category={"Bug"}
+            />
           </div>
-          <Category selected={selected} onClick={categoryHandler} category={"Feature"} />
+          <Category
+            selected={selected}
+            onClick={categoryHandler}
+            category={"Feature"}
+          />
         </div>
       </div>
     </div>
   );
 };
 
+SideBar.propTypes = {
+  showSideBar: PropTypes.bool,
+};
 export default SideBar;
--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -9,14 +9,14 @@ import { useContext, useState } from "react";
 import { FeedbackContext } from "../context/FeedbackContext";
 import SideBar from "../components/SideBar";
 const LandingScreen = () => {
-  const [showSideBar, setShowSideBar] = useState(false)
-  const { sortedState, loading } = useContext(FeedbackContext)
+  const [showSideBar, setShowSideBar] = useState(false);
+  const { sortedState, loading } = useContext(FeedbackContext);
   const sideBarHandler = () => {
-    setShowSideBar(!showSideBar)
-  }
+    setShowSideBar(!showSideBar);
+  };
   return (
-    <div className="h-screen">
-      <header className="relative flex px-10 py-6 justify-between items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="min-h-screen">
+      <header className=" flex px-10 py-6 justify-between items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
         <div className="flex flex-col gap-2">
           <h2 className="font-semibold text-2xl text-white">Frontend Mentor</h2>
           <h4 className="font-semibold text-xl text-grayTheme">
@@ -24,10 +24,14 @@ const LandingScreen = () => {
           </h4>
         </div>
         <div onClick={sideBarHandler}>
-          {!showSideBar ? <FaBars className="text-white text-4xl cursor-pointer" /> : <AiOutlineClose className="text-white text-4xl cursor-pointer"/>}
+          {!showSideBar ? (
+            <FaBars className="text-white text-4xl cursor-pointer" />
+          ) : (
+            <AiOutlineClose className="text-white text-4xl cursor-pointer" />
+          )}
         </div>
-        {showSideBar && <SideBar />}
       </header>
+      <div className="relative">{<SideBar showSideBar={showSideBar}/>}</div>
       <section className="flex justify-between bg-blueBlackTheme py-5 px-3">
         <SortDropDown />
         <CustomButton
@@ -37,41 +41,36 @@ const LandingScreen = () => {
         />
       </section>
       <main className="bg-grayTheme py-10 px-4 ">
-        {
-          loading === true ? (
-            <></>
-          ) : (
-            sortedState?.length === 0 ? (
-              <div className="bg-white py-20 rounded-lg">
-                <div className="flex gap-10 flex-col justify-center items-center">
-                  <img
-                    className="h-36 w-36"
-                    src={detective}
-                    alt="searching for results"
-                  />
-                  <h2 className="text-blueBlackTheme text-3xl font-bold">
-                    There is no feedback yet
-                  </h2>
+        {loading === true ? (
+          <></>
+        ) : sortedState?.length === 0 ? (
+          <div className="bg-white py-20 rounded-lg">
+            <div className="flex gap-10 flex-col justify-center items-center">
+              <img
+                className="h-36 w-36"
+                src={detective}
+                alt="searching for results"
+              />
+              <h2 className="text-blueBlackTheme text-3xl font-bold">
+                There is no feedback yet
+              </h2>
 
-                  <blockquote className="max-w-sm text-center text-lg">
-                    Got a suggestion? Found a bug that needs to be squashed? We love
-                    hearing about new ideas to improve our app.
-                  </blockquote>
-                  <CustomButton
-                    icon={<FaPlus />}
-                    text={"Add Feedback"}
-                    color={"#AD1FEA"}
-                  />
-                </div>
-              </div>
-            ) : (
-              sortedState?.map((suggestion) => {
-                return <Suggestion key={suggestion.id} suggestion={suggestion} />;
-              })
-            )
-          )
-        }
-
+              <blockquote className="max-w-sm text-center text-lg">
+                Got a suggestion? Found a bug that needs to be squashed? We love
+                hearing about new ideas to improve our app.
+              </blockquote>
+              <CustomButton
+                icon={<FaPlus />}
+                text={"Add Feedback"}
+                color={"#AD1FEA"}
+              />
+            </div>
+          </div>
+        ) : (
+          sortedState?.map((suggestion) => {
+            return <Suggestion key={suggestion.id} suggestion={suggestion} />;
+          })
+        )}
       </main>
     </div>
   );
