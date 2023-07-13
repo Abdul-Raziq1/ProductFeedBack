0a885b113af189c76050a75ea8379d0006898bd2
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-07-13 01:27:48 +0000

    Presentational categories shouldn't change when hovered on

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,14 +11,16 @@ const Category = ({ category, onClick, selected, isPresentational }) => {
     onClick(category);
     setSelected(true);
   };
+  const styleForFunctional = `${
+    filterBy === category && selected
+      ? "bg-blueTheme text-white tablet:hover:bg-blueTheme tablet:hover:bg-opacity-100"
+      : "bg-lighterBlueTheme bg-opacity-10 text-blueTheme tablet:hover:bg-blueTheme tablet:hover:bg-opacity-25"
+  } w-fit cursor-pointer py-1 px-5  font-semibold rounded-xl`
+  const styleForPresentational = "bg-lighterBlueTheme bg-opacity-10 text-blueTheme w-fit cursor-pointer py-1 px-5  font-semibold rounded-xl"
   return (
     <div
       onClick={() => handleClick(category)}
-      className={`${
-        filterBy === category && selected
-          ? "bg-blueTheme text-white tablet:hover:bg-blueTheme tablet:hover:bg-opacity-100"
-          : "bg-lighterBlueTheme bg-opacity-10 text-blueTheme tablet:hover:bg-blueTheme tablet:hover:bg-opacity-25"
-      } w-fit cursor-pointer py-1 px-5  font-semibold rounded-xl`}
+      className={isPresentational === true ? styleForPresentational : styleForFunctional}
     >
       <span>{category}</span>
     </div>
