3992e9977753b890ce345f3ce53504328b7b87fc
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-07-13 01:12:41 +0000

    Fixed bug causing weird hover colors in mobile view

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,18 +5,20 @@ import { FeedbackContext } from "../context/FeedbackContext";
 const Category = ({ category, onClick, selected, isPresentational }) => {
   const { setSelected, filterBy } = useContext(FeedbackContext);
   const handleClick = (category) => {
-    if (isPresentational){
-      return
+    if (isPresentational) {
+      return;
     }
     onClick(category);
-    setSelected(true)
+    setSelected(true);
   };
   return (
     <div
       onClick={() => handleClick(category)}
       className={`${
-        filterBy === category && selected ? "bg-blueTheme text-white hover:bg-blueTheme hover:bg-opacity-100" : "bg-lighterBlueTheme bg-opacity-10 text-blueTheme"
-      } w-fit cursor-pointer py-1 px-5  font-semibold rounded-xl ${isPresentational === undefined && "hover:bg-blueTheme hover:bg-opacity-25"}`}
+        filterBy === category && selected
+          ? "bg-blueTheme text-white tablet:hover:bg-blueTheme tablet:hover:bg-opacity-100"
+          : "bg-lighterBlueTheme bg-opacity-10 text-blueTheme"
+      } w-fit cursor-pointer py-1 px-5 tablet:hover:bg-blueTheme tablet:hover:bg-opacity-25  font-semibold rounded-xl`}
     >
       <span>{category}</span>
     </div>
@@ -27,6 +29,6 @@ Category.propTypes = {
   category: PropTypes.string,
   onClick: PropTypes.func,
   selected: PropTypes.bool,
-  isPresentational: PropTypes.bool
+  isPresentational: PropTypes.bool,
 };
 export default Category;
